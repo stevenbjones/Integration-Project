a36a8ff708e052877ec2bea4c9c90e74296ca5b7
Jensyboy <jensvandesteen123@gmail.com>
2020-05-01 12:22:14 +0200

    nieuw project aangemaakt

---
+++ Integration-project/ProjectSAI/ProjectSAI.DAL/ProjectSAIContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSAI.DAL
+{
+    class ProjectSAIContext
+    {
+    }
+}
---
+++ Integration-project/ProjectSAI/ProjectSAI/ConnectDatabase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSAI
+{
+    class ConnectDatabase
+    {
+        public static bool CheckDatabaseExists(string connString, string databaseName )
+        {
+            bool result = false;
+
+            SqlConnection sqlConnectionString = new SqlConnection(connString);
+            string sqlQuery = string.Format("SELECT database_id FROM sys.databases WHERE Name = '{0}'", databaseName);
+            using (SqlCommand sqlCmd = new SqlCommand(sqlQuery, sqlConnectionString))
+            {
+                sqlConnectionString.Open();
+                object resultObj = sqlCmd.ExecuteScalar();
+                int databaseID = 0;
+                if (resultObj != null)
+                {
+                    int.TryParse(resultObj.ToString(), out databaseID);
+                }
+                sqlConnectionString.Close();
+                result = (databaseID > 0);
+            }
+        }
+
+
+    }
