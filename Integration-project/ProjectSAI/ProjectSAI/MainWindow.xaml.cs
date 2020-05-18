93619d3834054d181eb34e876754d86c71a01569
Jarnodb <43603818+Jarnodb@users.noreply.github.com>
2020-05-18 16:18:05 +0200

    Optimized refreshing datagrid

--- Integration-project/ProjectSAI/ProjectSAI/MainWindow.xaml.cs
+++ Integration-project/ProjectSAI/ProjectSAI/MainWindow.xaml.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Win32;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -9,36 +8,20 @@ namespace ProjectSAI
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    /// 
+    ///
     public partial class MainWindow : Window
     {
-        Visibility _isGifLoadingVisible;
         public MainWindow()
         {
             InitializeComponent();
 
-            isGifLoadingVisible = Visibility.Hidden;
             ConnectDatabase.CreateDatabaseIfNotExists();
 
             ConnectDatabase.FillDataGrid(dtgStudent);
+        }
 
-        }
-        public Visibility isGifLoadingVisible
-        {
-            get => _isGifLoadingVisible;
-            set
-            {
-                if (_isGifLoadingVisible != value)
-                {
-                    _isGifLoadingVisible = value;
-                }
-            }
-        }
-     
         private void chkEditCells_Click(object sender, RoutedEventArgs e)
         {
-
-
             if (chkEditCells.IsChecked == true)
             {
                 dtgStudent.IsReadOnly = false;
@@ -53,48 +36,34 @@ namespace ProjectSAI
 
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
-            gifLoading.Visibility = Visibility.Visible;
             chkEditCells.IsChecked = false;
             btnSubmit.IsEnabled = false;
             dtgStudent.IsReadOnly = true;
             ConnectDatabase.UpdateDatabase(dtgStudent);
-            gifLoading.Visibility = Visibility.Hidden;
-
         }
 
         private void cmboPartnerLogo_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-
         }
 
-        void btnUploadData_Click(object sender, RoutedEventArgs e)
+        private async void btnUploadData_Click(object sender, RoutedEventArgs e)
         {
-         
             OpenFileDialog dlg = new OpenFileDialog();
-
             dlg.Filter = "CSV files (*.csv)|*.csv|XML files (*.xml)|*.xml";
 
             if (dlg.ShowDialog() == true)
             {
-                isGifLoadingVisible = Visibility.Visible;
                 string fileName;
                 fileName = dlg.FileName;
-                //MessageBox.Show(fileName);
-                
                 ConnectDatabase.UploadCSV(fileName, dtgStudent);
-                //MessageBox.Show("pls work");
-               
             }
-           
-            dtgStudent.Items.Refresh();
-            gifLoading.Visibility = Visibility.Hidden;
+            await Task.Run(() => this.Dispatcher.Invoke(() => ConnectDatabase.FillDataGrid(dtgStudent)));
+            MessageBox.Show("Items toegevoegd.", "", MessageBoxButton.OK, MessageBoxImage.Information);
         }
 
         private void btnGenerateRapport_Click(object sender, RoutedEventArgs e)
         {
             MaakWordDocument.Create();
         }
-
-
-    } }
-
+    }
+}
