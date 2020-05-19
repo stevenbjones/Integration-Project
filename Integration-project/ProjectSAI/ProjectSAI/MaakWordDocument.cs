874cd703103946c9ab03546aba69c2259e4df82a
stevenbjones <steven.bjones@student.odisee.be>
2020-05-19 17:20:06 +0200

    SPire libary

--- Integration-project/ProjectSAI/ProjectSAI/MaakWordDocument.cs
+++ Integration-project/ProjectSAI/ProjectSAI/MaakWordDocument.cs
@@ -2,7 +2,15 @@
 using System.Collections.Generic;
 using System.Windows;
 using Word = Microsoft.Office.Interop.Word;
-
+using Spire.Doc.Documents;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Office.Interop.Word;
+using Spire;
+using Spire.Doc.Documents;
+using Spire.Doc.Fields;
+using Spire.Doc.Interface;
+using Spire.Doc;
+using System.Data;
 
 namespace ProjectSAI
 {
@@ -11,78 +19,49 @@ namespace ProjectSAI
         public static void Create()
         {
             List<Leerling> leerlingen = ConnectDatabase.GetAllLeerlingenFromDatabase();
-            Word.Application WordApp = new Word.Application();
 
-            object fileName = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\Template2.docx";
+            Spire.Doc.Document testdoc = new Spire.Doc.Document();
+            testdoc.LoadFromFile(@"D:\test\Template2.docx");
 
-            object confirmConversions = Type.Missing;
+            Spire.Doc.Table table = new Spire.Doc.Table(testdoc, true);
 
-            object readOnly = Type.Missing;
-
-            object addToRecentFiles = Type.Missing;
-
-            object passwordDoc = Type.Missing;
-
-            object passwordTemplate = Type.Missing;
-
-            object revert = Type.Missing;
-
-            object writepwdoc = Type.Missing;
-
-            object writepwTemplate = Type.Missing;
-
-            object format = Type.Missing;
-
-            object encoding = Type.Missing;
-
-            object visible = Type.Missing;
-
-            object openRepair = Type.Missing;
-
-            object docDirection = Type.Missing;
-
-            object notEncoding = Type.Missing;
-
-            object xmlTransform = Type.Missing;
-            object missing = System.Reflection.Missing.Value;
-
-            Microsoft.Office.Interop.Word.Document doc = WordApp.Documents.Open(ref fileName, ref confirmConversions, ref readOnly, ref addToRecentFiles,
-
-            ref passwordDoc, ref passwordTemplate, ref revert, ref writepwdoc,
-
-            ref writepwTemplate, ref format, ref encoding, ref visible, ref openRepair,
-
-            ref docDirection, ref notEncoding, ref xmlTransform);
-
-            //////////////////////////////////////////////////
+            System.Data.DataTable dt = new System.Data.DataTable();
+            dt.Columns.Add("id", typeof(string));
+            dt.Columns.Add("name", typeof(string));
+            dt.Rows.Add(new string[] { "Stamnummer", "Nationaliteit "});
+            foreach (Leerling l in leerlingen)
+            {
+                dt.Rows.Add(new string[] { l.Stamnummer, l.Nationaliteit });
+            }
             
-            object start = 0;
-            object end = 0;
-            Word.Range tableLocation = doc.Range(ref start, ref end);
-            doc.Tables.Add(tableLocation, 3, 4);
 
-            doc.Tables[1].set_Style("Table Grid 8");
-            doc.Tables[1].Cell(2, 1).Range.Text = leerlingen[0].Stamnummer;
-            doc.Tables[1].Cell(2, 2).Range.Text = leerlingen[0].Thuistaal;
-            doc.Tables[1].Cell(2, 3).Range.Text = "Freaking";
-            doc.Tables[1].Cell(2, 4).Range.Text = "Awesome";
+            table.ResetCells(dt.Rows.Count, dt.Columns.Count);
 
-            
-            ReplaceBookmarkText(doc, "Dropouts", doc.Tables[1]);
-            Microsoft.Office.Interop.Word.Document document = WordApp.Documents.Add(ref missing, ref missing, ref missing, ref missing);
-            document = doc;
-            object filename = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + "\\exportData.docx";
-            doc.SaveAs2(ref filename);
-            doc.Close(ref missing, ref missing, ref missing);
-            doc = null;
-            WordApp.Quit(ref missing, ref missing, ref missing);
-            WordApp = null;
-            MessageBox.Show("Document created successfully !");
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                for (int j = 0; j < dt.Columns.Count; j++)
+                {
+                    table.Rows[i].Cells[j].AddParagraph().AppendText(dt.Rows[i][j].ToString());
+                }
+            }
+
+            BookmarksNavigator navigator = new BookmarksNavigator(testdoc);
+            navigator.MoveToBookmark("Dropouts");
+
+            TextBodyPart part = new TextBodyPart(testdoc);
+            part.BodyItems.Add(table);
+            navigator.ReplaceBookmarkContent(part);
+
+            testdoc.SaveToFile("output.docx", FileFormat.Docx2013);
+            System.Diagnostics.Process.Start("output.docx");
+
+
         }
 
         private static void ReplaceBookmarkText(Microsoft.Office.Interop.Word.Document doc, string bookmarkName, Word.Table text)
 
         {
+
 
             if (doc.Bookmarks.Exists(bookmarkName))
 
@@ -90,13 +69,17 @@ namespace ProjectSAI
 
                 Object name = bookmarkName;
 
+
+               object test = doc.Bookmarks.get_Item(ref name);
+
                 Microsoft.Office.Interop.Word.Range range = doc.Bookmarks.get_Item(ref name).Range;
 
                 range.Text = text.ToString();
 
+
                 object newRange = range;
 
-                doc.Bookmarks.Add(bookmarkName, ref newRange);
+               // doc.Bookmarks.Add(bookmarkName, ref newRange);
 
             }
 
