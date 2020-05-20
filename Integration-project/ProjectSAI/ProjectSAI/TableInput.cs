0b40c83afae1ba60301c9e4f41b0cbba780784ab
stevenbjones <steven.bjones@student.odisee.be>
2020-05-20 16:11:23 +0200

    Classe tabelInput

---
+++ Integration-project/ProjectSAI/ProjectSAI/TableInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectSAI
+{
+    public class TableInput
+    {
+        public string Group { get; set; }
+
+        public int Year { get; set; }
+
+        public int Value { get; set; }
+    }
+}
